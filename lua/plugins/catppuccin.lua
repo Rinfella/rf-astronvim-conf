45cb4b12b84d842187cea9afc0cc8947aeee4d5b
Rinfella <rinfellapachuau93@gmail.com>
2024-08-06 16:40:23 +0530

    updated community plugins

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      dim_inactive = { enabled = true, percentage = 0.25 },
+      highlight_overrides = {
+        mocha = function(c)
+          return {
+            Normal = { bg = c.mantle },
+          }
+        end,
+      },
+    },
+  },
+}
