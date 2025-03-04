0d3aaec6694e89d2acba8fd396491e27c866afbc
Rinfella <rinfellapachuau93@gmail.com>
2025-03-04 13:07:51 +0530

    updated inlay hints settings to true

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -60,17 +60,16 @@ return {
           desc = "Close buffer from tabline",
 
           -- Load the plugins/dash.luad if there is no buffer open:
-          
         },
 
         ["<Leader>c"] = {
-            function()
-              local bufs = vim.fn.getbufinfo { buflisted = true }
-              require("astrocore.buffer").close(0)
-              if require("astrocore").is_available "alpha-nvim" and not bufs[2] then require("alpha").start() end
-            end,
-            desc = "Close Buffer and return to default dashboard",
-          },
+          function()
+            local bufs = vim.fn.getbufinfo { buflisted = true }
+            require("astrocore.buffer").close(0)
+            if require("astrocore").is_available "alpha-nvim" and not bufs[2] then require("alpha").start() end
+          end,
+          desc = "Close Buffer and return to default dashboard",
+        },
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
